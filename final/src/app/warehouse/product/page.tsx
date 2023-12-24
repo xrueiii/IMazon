1233ef902fd66f34fc3dfc5c0bed89c55122db1e
xrueiii <b11705043@g.ntu.edu.tw>
2023-12-25 00:14:46 +0800

    branch test

---
+++ final/src/app/warehouse/product/page.tsx
@@ -0,0 +1,8 @@
+function ProductPage() {
+  return (
+    <div className="flex h-full w-full items-center justify-center rounded-b-xl border-2">
+      <div></div>
+    </div>
+  );
+}
+export default ProductPage;
