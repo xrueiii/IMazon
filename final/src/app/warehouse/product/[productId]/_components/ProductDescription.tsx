cead6c64da0cd62588c92c03a7cca2cf5e71036a
xrueiii <b11705043@g.ntu.edu.tw>
2023-12-29 03:44:33 +0800

    textarea

--- final/src/app/warehouse/product/[productId]/_components/ProductDescription.tsx
+++ final/src/app/warehouse/product/[productId]/_components/ProductDescription.tsx
@@ -1,35 +1,35 @@
-import { getProductDetail_1, getProductPhotos } from "./actions";
 import Image from "next/image";
 
+import { getProductDetail_1, getProductPhotos } from "./actions";
+
 type Props = {
-    productId: string; 
-}
+  productId: string;
+};
 
+export default async function ProductDescription({ productId }: Props) {
+  const productDetail1 = await getProductDetail_1(productId);
+  const productPhotos = await getProductPhotos(productId);
 
-export default async function ProductDescription( { productId }: Props) {
-    const productDetail1 = await getProductDetail_1(productId);
-    const productPhotos = await getProductPhotos(productId);
-
-    
-    return(
-        <div
-        className="min-h-0 w-3/5 grow overflow-y-scroll  p-8 text-lg"
-        style={{ lineHeight: "1.8" }}
-      >
-        <div className="text-3xl font-medium">產品介紹：</div>
-        <br />
-        {productDetail1.productDescription.split("\n").map((line, index) => (
-          <p key={index}>{line}</p>
-        ))}
-        <br />
-        {productPhotos.map((photo, index) => 
-            <Image
-            key={index}
-            src={photo.productImageLink}
-            alt="DescProduct photo"
-            width={350}
-            height={350}
-            />)}
-        
-      </div>)
+  return (
+    <div
+      className="min-h-0 w-3/5 grow overflow-y-scroll  p-8 text-lg"
+      style={{ lineHeight: "1.8" }}
+    >
+      <div className="text-3xl font-medium">產品介紹：</div>
+      <br />
+      <div className="whitespace-pre-line">
+        {productDetail1.productDescription}
+      </div>
+      <br />
+      {productPhotos.map((photo, index) => (
+        <Image
+          key={index}
+          src={photo.productImageLink}
+          alt="DescProduct photo"
+          width={350}
+          height={350}
+        />
+      ))}
+    </div>
+  );
 }
