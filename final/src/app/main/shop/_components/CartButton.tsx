1c0b0e07c1f506d87ab5658eb45885b977811246
xrueiii <b11705043@g.ntu.edu.tw>
2023-12-30 01:56:52 +0800

    cart schema

--- final/src/app/main/shop/_components/CartButton.tsx
+++ final/src/app/main/shop/_components/CartButton.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { IconButton } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export default function CartButton() {
-    return (
-        <IconButton>
-            <ShoppingCartIcon/>
-        </IconButton>
-    );
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push("/main/shop/cart");
+  };
+  return (
+    <IconButton onClick={handleOnClick}>
+      <ShoppingCartIcon />
+    </IconButton>
+  );
 }
--- final/src/app/main/shop/cart/_components/GoShoppingButton.tsx
+++ final/src/app/main/shop/cart/_components/GoShoppingButton.tsx
@@ -2,17 +2,17 @@
 
 import { useRouter } from "next/navigation";
 
-export default function AddProductButton() {
+export default function GoShoppingButton() {
   const router = useRouter();
   const handleOnClick = () => {
-    router.push("/main/warehouse/AddProduct");
+    router.push("/main/shop");
   };
   return (
     <button
       onClick={handleOnClick}
-      className="rounded-full px-2 text-4xl hover:bg-slate-100"
+      className="h-12 rounded-md border-2 bg-teal-900 px-4 text-white hover:bg-teal-800 "
     >
-      +
+      Go Shopping
     </button>
   );
 }
