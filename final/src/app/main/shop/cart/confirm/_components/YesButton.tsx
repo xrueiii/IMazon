32ae4a1c1880785837eb3ef00c9c79610721bc09
xrueiii <b11705043@g.ntu.edu.tw>
2023-12-30 15:44:43 +0800

    Clean Cart

--- final/src/app/main/shop/cart/confirm/_components/YesButton.tsx
+++ final/src/app/main/shop/cart/confirm/_components/YesButton.tsx
@@ -1,18 +1,55 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 
-export default function YesButton() {
+import useCart from "@/hooks/useCart";
+import { publicEnv } from "@/lib/env/public";
+
+type Props = {
+  carts: {
+    displayId: string;
+  }[];
+};
+
+function YesButton({ carts }: Props) {
+  const session = useSession();
+  const userId = session?.data?.user?.id;
+  if (!userId) {
+    redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}`);
+  }
   const router = useRouter();
-  const handleOnClick = () => {
-    router.push("/main/shop/cart/confirm/success");
+  const { deleteCart } = useCart();
+
+  const handleDelete = async () => {
+    try {
+      for (let i = 0; i < carts.length; i++) {
+        await deleteCart(carts[i].displayId);
+      }
+
+      router.push(
+        `${publicEnv.NEXT_PUBLIC_BASE_URL}/main/shop/cart/confirm/success`,
+      );
+    } catch (error) {
+      console.error("Error deleting carts:", error);
+    }
   };
+
   return (
-    <button
-      onClick={handleOnClick}
-      className="h-12 rounded-md border-2 bg-teal-900 px-4 text-white hover:bg-teal-800 "
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleDelete();
+      }}
     >
-      Confirm
-    </button>
+      <button
+        type={"submit"}
+        className="h-12 rounded-md border-2 bg-teal-900 px-4 text-white hover:bg-teal-800 "
+      >
+        Confirm
+      </button>
+    </form>
   );
 }
+
+export default YesButton;
